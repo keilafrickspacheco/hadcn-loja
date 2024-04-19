f6928495887c6edfd71ede4ffde8598bd7199a70
keilafrickspacheco <keyla.fricks@outlook.com>
2024-04-19 05:35:05 -0300

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Footer } from "@/components/footer"
+import { Header } from "@/components/header"
+
+const Page = () => {
+  return (
+    <div className="w-full max-w-4xl mx-auto">
+      <Header/>
+      <div className="mx-3">...</div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/components/logo.tsx
@@ -0,0 +1,5 @@
+export const Logo = () => {
+    return (
+        <div className="text-xl">Loja <spa className="font-bold">ZAP</spa></div>
+    )
+}
